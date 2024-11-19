783da817ac38ed76ef9c3fa265db11db25642690
Felix Zhang <fe1ix@fb.com>
2024-11-18 16:53:07 -0800

    chapter3

--- src/bullets.tsx
+++ src/bullets.tsx
@@ -5,19 +5,27 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { Quaternion, Vector3 } from "three";
+import { Mesh, Quaternion, Vector3 } from "three";
 
 import { create } from "zustand";
 import { generateUUID } from "three/src/math/MathUtils.js";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const bulletSpeed = 10;
+const forwardVector = new Vector3(0, 0, -1);
+const bulletTimeToLive = 2;
 
 type BulletData = {
   id: string;
   initPosition: Vector3;
   initQuaternion: Quaternion;
+  timestamp: number;
 };
 type BulletStore = {
   bullets: BulletData[];
   addBullet: (position: Vector3, quaternion: Quaternion) => void;
+  removeBullet: (bulletId: string) => void;
 };
 export const useBulletStore = create<BulletStore>((set) => ({
   bullets: [],
@@ -27,9 +35,17 @@ export const useBulletStore = create<BulletStore>((set) => ({
         id: generateUUID(),
         initPosition: position,
         initQuaternion: quaternion,
+        timestamp: performance.now(),
       };
+      setTimeout(() => {
+        state.removeBullet(newBullet.id);
+      }, bulletTimeToLive * 1000);
       return { bullets: [...state.bullets, newBullet] };
     }),
+  removeBullet: (bulletId) =>
+    set((state) => ({
+      bullets: state.bullets.filter((bullet) => bullet.id !== bulletId),
+    })),
 }));
 
 export const Bullets = () => {
@@ -47,11 +63,24 @@ type BulletProps = {
   bulletData: BulletData;
 };
 const Bullet = ({ bulletData }: BulletProps) => {
+  const ref = useRef<Mesh>(null);
+
+  useFrame(() => {
+    const now = performance.now();
+    const bulletObject = ref.current!;
+    const directionVector = forwardVector
+      .clone()
+      .applyQuaternion(bulletObject.quaternion);
+    bulletObject.position.addVectors(
+      bulletData.initPosition,
+      directionVector.multiplyScalar(
+        (bulletSpeed * (now - bulletData.timestamp)) / 1000
+      )
+    );
+  });
+
   return (
-    <mesh
-      position={bulletData.initPosition}
-      quaternion={bulletData.initQuaternion}
-    >
+    <mesh ref={ref} quaternion={bulletData.initQuaternion}>
       <sphereGeometry args={[0.02]} />
       <meshStandardMaterial color="grey" />
     </mesh>
